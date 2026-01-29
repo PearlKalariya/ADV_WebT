6b90b053da8e30c4be1b54fd6a03677c8bf72890
PearlKalariya <pearlkalariyaofficial@gmail.com>
2026-01-29 18:37:06 +0530

    123

--- index2.js
+++ index2.js
@@ -4,12 +4,12 @@ const fs=require("fs");
 const myServer=http.createServer((req,res)=>{
     const log=`${Date.now()}: ${req.url} New request recieved\n`;
     fs.appendFile("log.txt",log,(err,data)=>{
-        res.end("Hello from server again");
+        res.end("Hello from server again hellooooooo !!!!!!!!!!!!!!!!!!!!!!!!");
     })
     
-    res.end("Hello from server");
+    // res.end("Hello from server agai");
 });
 
-myServer.listen(8000,()=>{
+myServer.listen(5000,()=>{
     console.log("Server started");
 });
---
+++ url/index.js
@@ -0,0 +1,53 @@
+import http from 'http';
+import url from 'url';
+import logger from './logger.js';
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/favicon.ico') {
+    res.writeHead(204);
+    res.end();
+    return;
+  }
+
+  // Parse the URL
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+  console.log('Parsed URL:');
+  console.log('  Pathname:', pathname);
+  console.log('  Query:', query);
+
+  logger(req, res);
+
+  switch (pathname) {
+    case '/':
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Welcome to the Home Page!\n');
+      break;
+    case '/about':
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('This is the About Page.\n');
+      break;
+    case '/search':
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({
+        message: 'Search page',
+        queryParameters: query
+      }, null, 2));
+      break;
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found\n');
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, (err, _) => {
+  if (err) {
+    console.error('Error starting server:', err);
+    return;
+  }
+
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
